f24d786759c6dc0d7b6c5f932c1e16d260b29045
YangHangzheng <1045730885@qq.com>
2018-10-26 16:31:30 +0800

    java

---
+++ timeaxis/src/main/java/com/yanghangzheng/timeaxis/TimeaxisApplication.java
@@ -0,0 +1,12 @@
+package com.yanghangzheng.timeaxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeaxisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeaxisApplication.class, args);
+	}
+}
